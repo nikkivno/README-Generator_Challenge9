f0cafe56fc75651c7642dc844a734272f13ff9b7
nikkivno <nikkivigneault@gmail.com>
2023-06-29 12:35:30 -0300

    read/write file functions added

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 inquirer
@@ -12,7 +13,12 @@ inquirer
     {
         type: 'input',
         message: 'What is your project description?',
-        name: 'Project Description',
+        name: 'Description',
+    },
+      {
+        type: 'input',
+        message:'Please list the table of contents:',
+        name:'Table of Contents',
     },
     {
         type: 'input',
@@ -27,17 +33,31 @@ inquirer
     {
         type: 'input',
         message:'Do you have any collaborators to credit?',
-        name:'Credits',
+        name:'Contributing',
     },
     {
         type: 'input',
         message:'What license are you using for this project?',
         name:'License',
     },
+        {
+        type: 'input',
+        message:'Please include test instructions:',
+        name:'Tests',
+    },
+
 ])
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
+
+fs.readFile('inquirer', 'uft8', (error, data) =>
+error ? console.error(error) : console.log(data)
+);
+
+fs.writeFile('README.md', process.argv[2], (error) =>
+error ? console.error(error) : console.log('Success!')
+);
 
 // TODO: Create a function to initialize app
 function init() {}
