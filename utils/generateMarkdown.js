112120adde5f24a59a188dc0028f6f7cf896e431
nikkivno <nikkivigneault@gmail.com>
2023-07-03 00:48:57 -0300

    first try at renderlicenselink and renderlicensesection functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -29,12 +29,62 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'MIT License') {
+    return `## License
+    
+    This project is licensed under the [MIT License] ([MIT License](https://opensource.org/licenses/MIT).`; 
+  } else if (license === 'Apache 2.0') {
+    return `## License 
 
-// TODO: Create a function to generate markdown for README
+    This project is license under the [Apache License 2.0](https://opensource.org/license/apache-2-0/)`
+  } else if (license === 'BSD 3-Clause License') {
+    return `## License
+
+This project is licensed under the [BSD 3-Clause License](https://opensource.org/licenses/BSD-3-Clause).`;
+  } else if (license === 'Mozilla Public License 2.0') {
+    return `## License
+
+This project is licensed under the [Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0).`;
+  } else {
+    return ''; // Empty string if there is no license
+  }
+}
+
+// // TODO: Create a function to generate markdown for README
+// function generateMarkdown(data) {
+//   return `# ${data.title}
+
+// `;
+// }
+
+// module.exports = generateMarkdown;
+
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.ProjectTitle}
 
+## Description
+${data.Description}
+
+## Installation
+${data.Installation}
+
+## Usage
+${data.Usage}
+
+## Contributing
+${data.Contributing}
+
+## License
+${data.License}
+
+## Tests
+${data.Tests}
+
+## Questions
+If you have any questions, you can reach out to me at:
+Github: ${data.Questions}
+Email: ${data.Email}
 `;
 }
 
